386284db552f741834912632845241e676c765e0
malikharis1 <harismalik1@protonmail.com>
2022-10-04 14:24:31 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class",
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    screens: {
+      'sm': '100px',
+      // => @media (min-width: 576px) { ... }
+
+      'md': '460px',
+      // => @media (min-width: 960px) { ... }
+
+      'lg': '1440px',
+      // => @media (min-width: 1440px) { ... }
+    },
+    extend: {
+      fontFamily: {
+        burtons: "burtons",
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
